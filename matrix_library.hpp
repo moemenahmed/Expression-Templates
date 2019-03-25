cbb5ce3521fb1b606f1331a6d9696a64c609e06e
Moamen Ahmed <moamen.ahmed1@hotmail.com>
2019-03-25 03:46:54 +0200

    CRTP used , one Matrix class

--- matrix_library.hpp
+++ matrix_library.hpp
@@ -1,5 +1,5 @@
-/* Matrix Library : a library that deals with matrices by expression templates , performs 
-operations like elementwise addition, subtraction, scalar multiplication and 
+/* Matrix Library : a library that deals with matrices by expression templates , performs
+operations like elementwise addition, subtraction, scalar multiplication and
 matrix multipication operations using operators .
 it also supports different numerical tybes of a matrix like (int, long, float, double, complex,..)
 
@@ -9,7 +9,7 @@ it also supports different numerical tybes of a matrix like (int, long, float, d
 #include <vector>
 #include <functional>
 
-/* 
+/*
 Scalar class :  class for objects that represent scalars of any type (int , double , complex ,...)
 */
 template <typename T>
@@ -23,7 +23,7 @@ public:
 	{
 	}
 	// for index operations the scalar is the value of each element 
-	T operator() (size_t,size_t) const
+	T operator() (size_t, size_t) const
 	{
 		return s;
 	}
@@ -42,32 +42,69 @@ public:
 
 /* helper Traits class to select how to refer to an expression template node
   - in general : by reference
-  - for scalars : by value 
+  - for scalars : by value
  */
-template <typename T> 
+template <typename T>
 class Traits
 {
 public:
 	typedef T const& ExprRef; // type to refer to is constant reference
 };
 // partial specialization for scalars 
-template <typename T> 
-class Traits<Scalar<T> > 
-{ 
+template <typename T>
+class Traits<Scalar<T> >
+{
 public:
-typedef Scalar<T> ExprRef; // type to refer to is ordinary value 
+	typedef Scalar<T> ExprRef; // type to refer to is ordinary value 
 };
+
+
 
 /*****************************expression class template***************************************/
 
-/* 
-Operation : expression template class for objects that represent the operation 
-of two operands. 
-The operands can be matrix or scalar or Operation again (nested expressions).
-The Operation class is used for both addition and multiplication using generic lambdas.
+/**************************************Expression class***************************************/
+/*
+Expression : is the base class (CRTP) , it can be a simple matrix or an operation that contains another
+matrices or another operations.
 */
-template <typename T, typename OP1, typename OP2> 
-class Operation
+template <typename Type,typename T> // Type is either Matrix or Operation
+class Expression
+{
+public:
+
+	// size of represented data 
+	size_t get_rows() const
+	{
+		return static_cast<Type const&>(*this).get_rows();
+	}
+	size_t get_cols() const
+	{
+		return static_cast<Type const&>(*this).get_cols();
+	}
+
+
+	// index element operator for constants 
+	T operator() (const size_t r, const size_t c) const
+	{
+		return static_cast<Type const&>(*this)(r, c);
+	}
+	// index element operator for variables
+	T& operator() (const size_t r, const size_t c)
+	{
+		return static_cast<Type const&>(*this)(r, c);
+	}
+
+
+};
+
+/*
+Operation : expression template class for objects that represent the operation
+of two operands.
+The operands can be matrix or scalar or Operation again (nested expressions).
+The Operation class is used for both addition and multiplication using lambdas.
+*/
+template <typename T, typename OP1, typename OP2>
+class Operation:public Expression<Operation<T,OP1,OP2>,T>
 {
 private:
 	typename Traits<OP1>::ExprRef op1; // first operand 
@@ -75,17 +112,17 @@ private:
 	std::function <T(T, T)>operation;
 public:
 	// constructor initializes references to operands
-	Operation (OP1 const& a, OP2 const& b, const std::function<T(T, T)>& _operation) : op1(a), op2(b)
+	Operation(OP1 const& a, OP2 const& b, const std::function<T(T, T)>& _operation) : op1(a), op2(b)
 	{
 		operation = _operation;
 	}
 	// compute sum when value requested 
-	T operator() (const size_t r,const size_t c) const 
-	{ 
-		return operation(op1(r,c) , op2(r,c));
+	T operator() (const size_t r, const size_t c) const
+	{
+		return operation(op1(r, c), op2(r, c));
 	}
 	// size is maximum size 
-	size_t get_rows() const 
+	size_t get_rows() const
 	{
 		//size 0 is for Scalar
 		assert(op1.get_rows() == 0 || op2.get_rows() == 0 || op1.get_rows() == op2.get_rows());
@@ -105,8 +142,8 @@ public:
 Matrix : a simple matrix class without the use of expression templates.
 It is used for the next main class .
 */
-template <typename T> 
-class Matrix
+template <typename T>
+class Matrix:public Expression<Matrix<T>,T>
 {
 private:
 	std::vector<std::vector<T> > matrix;
@@ -114,7 +151,7 @@ private:
 	size_t cols;
 public:
 	// create matrix with initial size
-	Matrix(const size_t r , const size_t c)
+	Matrix(const size_t r, const size_t c)
 	{
 		matrix.resize(r);
 		for (size_t i = 0; i < matrix.size(); i++) {
@@ -124,12 +161,12 @@ public:
 		cols = c;
 	}
 	// create matrix with initial size and initial value
-	Matrix(const size_t r, const size_t c , const T initial_value)
+	Matrix(const size_t r, const size_t c, const T initial_value)
 	{
 		matrix.resize(r);
-		for (size_t i = 0; i < matrix.size(); i++) 
+		for (size_t i = 0; i < matrix.size(); i++)
 		{
-			matrix[i].resize(c,initial_value);
+			matrix[i].resize(c, initial_value);
 		}
 		rows = r;
 		cols = c;
@@ -148,7 +185,7 @@ public:
 		{
 			return *this;
 		}
-		
+
 		matrix.resize(M.rows);
 		for (size_t i = 0; i < matrix.size(); i++) {
 			matrix[i].resize(M.cols);
@@ -164,7 +201,23 @@ public:
 
 		return *this;
 	}
-	
+	//assignment operator for Expression calculating
+	template<typename Type>
+	Matrix & operator=(Expression<Type,T> const& M)
+	{
+		size_t rows = M.get_rows();
+		size_t cols = M.get_cols();
+		assert(get_rows() == rows && get_cols() == cols);
+		for (size_t r = 0; r < rows; r++)
+		{
+			for (size_t c = 0; c < cols; c++)
+			{
+				(*this)(r, c) = M(r, c);
+			}
+
+		}
+		return *this;
+	}
 	//get number of rows
 	size_t get_rows()const
 	{
@@ -177,12 +230,12 @@ public:
 	}
 
 	//index vector operator for constants
-	std::vector<T> operator[] (const size_t r) const 
+	std::vector<T> operator[] (const size_t r) const
 	{
 		return matrix[r];
 	}
 	//index vector operator for variables
-	std::vector<T>& operator[] (const size_t r) 
+	std::vector<T>& operator[] (const size_t r)
 	{
 		return matrix[r];
 	}
@@ -196,7 +249,25 @@ public:
 	{
 		return matrix[r][c];
 	}
-
+	// operator +=
+	template <typename T>
+	Matrix<T>& operator += (const Matrix<T> & a)
+	{
+		Matrix<T> output(get_rows(), get_cols());
+		output = (*this) + a;
+		(*this) = output;
+		return (*this);
+	}
+	// operator -=
+	template <typename T>
+	Matrix<T>& operator -= (const Matrix<T> & a)
+	{
+		Matrix<T> output(get_rows(), get_cols());
+		output = (*this) - a;
+		(*this) = output;
+		return (*this);
+	}
+	//print matrix to console
 	void print_matrix()
 	{
 		std::cout << std::endl;
@@ -214,173 +285,59 @@ public:
 	}
 };
 
-/**************************************ExpMatrix class***************************************/
-/*
-ExpMatrix : is the main class , it can be a simple matrix or an operation that contains another
-matrices or another operations.
-The user should define a matrix with the ExpMatrix class.
-*/
-template <typename T, typename obj = Matrix<T> > //obj type is either Matrix or Operation
-class ExpMatrix
-{
-private:
-	obj exp_obj; // access to the data of the matrix
-public:
-	// create matrix with initial size
-	ExpMatrix(const size_t r, const size_t c):exp_obj(r,c)
-	{
-	}
-	// create matrix with initial size and initial value
-	ExpMatrix(const size_t r, const size_t c, const T initial_value):exp_obj(r,c, initial_value)
-	{
-	}
-	// copy constructor
-	ExpMatrix(const obj& M):exp_obj(M)
-	{
-	}
-	// size of represented data 
-	size_t get_rows() const
-	{ 
-		return exp_obj.get_rows();
-	}
-	size_t get_cols() const
-	{
-		return exp_obj.get_cols();
-	}
-	// assignment operator for same type 
-	ExpMatrix& operator= (ExpMatrix const& M) 
-	{ 
-		size_t rows = M.get_rows();
-		size_t cols = M.get_cols();
-		assert(get_rows() == rows && get_cols() == cols);
-		for (size_t r = 0; r < rows; r++)
-		{
-			for (size_t c = 0; c < cols; c++)
-			{
-				exp_obj(r, c) = M(r, c);
-			}
 
-		}
-		return *this;
-	}
-
-	// assignment operator for different type 
-	template<typename T2, typename obj2> 
-	ExpMatrix& operator= (ExpMatrix<T2, obj2> const& M) 
-	{  
-		size_t rows = M.get_rows();
-		size_t cols = M.get_cols();
-		assert(get_rows() == rows && get_cols() == cols);
-		for (size_t r = 0; r < rows; r++)
-		{
-			for (size_t c = 0; c < cols; c++)
-			{
-				exp_obj(r, c) = M(r, c);
-			}
-
-		}
-		return *this;
-	}
-
-	// index element operator for constants 
-	T operator() (const size_t r, const size_t c) const
-	{ 
-		assert(r<get_rows() || c<get_cols()); 
-		return exp_obj(r,c);
-	}
-	// index element operator for variables
-	T& operator() (const size_t r, const size_t c)
-	{
-		assert(r < get_rows() || c < get_cols());
-		return exp_obj(r,c);
-	}
-	// return what the matrix currently represents 
-	obj const& object() const 
-	{ 
-		return exp_obj;
-	} 
-	obj& object() 
-	{ 
-		return exp_obj; 
-	}
-
-	// operator +=
-	template <typename T>
-	ExpMatrix<T>& operator += (const ExpMatrix<T> & a)
-	{
-		ExpMatrix<T> output (get_rows(), get_cols());
-		output = (*this) + a;
-		(*this) = output;
-		return (*this);
-	}
-	// operator -=
-	template <typename T>
-	ExpMatrix<T>& operator -= (const ExpMatrix<T> & a)
-	{
-		ExpMatrix<T> output(get_rows(), get_cols());
-		output = (*this) - a;
-		(*this) = output;
-		return (*this);
-	}
-	void print_matrix()
-	{
-		exp_obj.print_matrix();
-	}
-};
 
 /******************************************operators********************************************/
 // addition of two matrices
-template <typename T, typename R1, typename R2> 
-ExpMatrix<T, Operation<T, R1, R2> > operator+ (ExpMatrix<T, R1> const& a, ExpMatrix<T, R2> const& b) 
+template <typename T, typename R1, typename R2>
+auto  operator+ (Expression<R1,T> const& a, Expression<R2, T> const& b)
 {
 	auto f = [](T x, T y) {return x + y; };
-	return ExpMatrix<T, Operation<T, R1, R2> >(Operation<T, R1, R2>(a.object(), b.object(),f ));
+	return Operation<T, Expression<R1, T>, Expression<R2, T>> (a, b, f);
 }
 // subtraction of two matrices
 template <typename T, typename R1, typename R2>
-ExpMatrix<T, Operation<T, R1, R2> > operator- (ExpMatrix<T, R1> const& a, ExpMatrix<T, R2> const& b)
+auto  operator- (Expression<R1, T> const& a, Expression<R2, T> const& b)
 {
 	auto f = [](T x, T y) {return x - y; };
-	return ExpMatrix<T, Operation<T, R1, R2> >(Operation<T, R1, R2>(a.object(), b.object(), f));
+	return Operation<T, Expression<R1, T>, Expression<R2, T>>(a, b, f);
 }
-	
-
 
 // scalar multiplication ( scalar * matrix)
 template <typename T, typename R2>
-ExpMatrix<T, Operation<T, Scalar<T>, R2> > operator* (const T& a, ExpMatrix<T, R2> const& b)
+auto operator* (const T& a, Expression<R2, T> const& b)
 {
 	auto f = [](T x, T y) {return x * y; };
-	return ExpMatrix<T, Operation<T, Scalar<T>, R2> >(Operation<T, Scalar<T>, R2>(Scalar<T>(a), b.object(), f));
+	return Operation<T, Scalar<T>, Expression<R2, T>> (Scalar<T>(a), b, f);
 }
 // scalar multiplication ( matrix * scalar)
 template <typename T, typename R2>
-ExpMatrix<T, Operation<T, Scalar<T>, R2> > operator* (ExpMatrix<T, R2> const& b, const T& a)
+auto  operator* (Expression<R2, T> const& b, const T& a)
 {
 	auto f = [](T x, T y) {return x * y; };
-	return ExpMatrix<T, Operation<T, Scalar<T>, R2> >(Operation<T, Scalar<T>, R2>(Scalar<T>(a), b.object(), f));
+	return Operation<T, Scalar<T>, Expression<R2, T>> (Scalar<T>(a), b, f);
 }
 // matrix multiplication
 template <typename T>
-ExpMatrix<T> operator* (ExpMatrix<T> const& a, ExpMatrix<T> const& b)
+Matrix<T> operator* (Matrix<T> const& a, Matrix<T> const& b)
 {
 
-	assert(a.get_cols()==b.get_rows());
-	ExpMatrix<T> output(a.get_rows(), b.get_cols(),0);
+	assert(a.get_cols() == b.get_rows());
+	Matrix<T> output(a.get_rows(), b.get_cols(), 0);
 	size_t rows = output.get_rows();
 	size_t cols = output.get_cols();
 	for (size_t i = 0; i < rows; i++)
 	{
 		for (size_t j = 0; j < cols; j++)
 		{
-			for (size_t k = 0; k < rows;k++)
+			for (size_t k = 0; k < rows; k++)
 			{
 				output(i, j) += a(i, k)*b(k, j);
 			}
-			
+
 		}
 	}
 	return output;
-	
+
 }
 
--- testing_matrix_library.cpp
+++ testing_matrix_library.cpp
@@ -13,11 +13,11 @@ void example1()
 	std::cout << "example 1: " << std::endl;
 	size_t rows = 1000;
 	size_t cols = 1000;
-	ExpMatrix<int> A(rows, cols, 4);// matrix with all elements equal 4
-	ExpMatrix<int> B(rows, cols, 1);// matrix with all elements equal 1
-	ExpMatrix<int> C(rows, cols, 3);// matrix with all elements equal 3
-	ExpMatrix<int> D(rows, cols, 5);// matrix with all elements equal 5
-	ExpMatrix<int> sum(rows, cols);// matrix with all elements equal 0
+	Matrix<int> A(rows, cols, 4);// matrix with all elements equal 4
+	Matrix<int> B(rows, cols, 1);// matrix with all elements equal 1
+	Matrix<int> C(rows, cols, 3);// matrix with all elements equal 3
+	Matrix<int> D(rows, cols, 5);// matrix with all elements equal 5
+	Matrix<int> sum(rows, cols);// matrix with all elements equal 0
 
 	auto start = std::chrono::high_resolution_clock::now();
 	sum = A + B + C + D;
@@ -27,8 +27,10 @@ void example1()
 	std::cout << "one sample entry of the output matrix : " << sum(0, 0) << std::endl;
 	std::cout << std::endl;
 }
+
 void example2()
 {
+	
 	// example 2 : add 3 matrices 500x500 of complex type
 	std::cout << "example 2: " << std::endl;
 	size_t rows = 500;
@@ -36,10 +38,10 @@ void example2()
 	std::complex<int>c1(5, 6);
 	std::complex<int>c2(1, 0);
 	std::complex<int>c3(0, 5);
-	ExpMatrix<std::complex<int>> A(rows, cols, c1);// matrix with all elements equal 5+i*6
-	ExpMatrix<std::complex<int>> B(rows, cols, c2);// matrix with all elements equal 1
-	ExpMatrix<std::complex<int>> C(rows, cols, c3);// matrix with all elements equal i*5
-	ExpMatrix<std::complex<int>> sum(rows, cols);// matrix with all elements equal 0
+	Matrix<std::complex<int>> A(rows, cols, c1);// matrix with all elements equal 5+i*6
+	Matrix<std::complex<int>> B(rows, cols, c2);// matrix with all elements equal 1
+	Matrix<std::complex<int>> C(rows, cols, c3);// matrix with all elements equal i*5
+	Matrix<std::complex<int>> sum(rows, cols);// matrix with all elements equal 0
 
 	auto start = std::chrono::high_resolution_clock::now();
 	sum = A + B + C ;
@@ -55,11 +57,11 @@ void example3()
 	std::cout << "example 3: " << std::endl;
 	size_t rows = 500;
 	size_t cols = 500;
-	ExpMatrix<double> A(rows, cols, 4.1);// matrix with all elements equal 4.1
-	ExpMatrix<double> B(rows, cols, 1.2);// matrix with all elements equal 1.2
-	ExpMatrix<double> C(rows, cols, 3.6);// matrix with all elements equal 3.6
-	ExpMatrix<double> D(rows, cols, 5.3);// matrix with all elements equal 5.3
-	ExpMatrix<double> sum(rows, cols);// matrix with all elements equal 0
+	Matrix<double> A(rows, cols, 4.1);// matrix with all elements equal 4.1
+	Matrix<double> B(rows, cols, 1.2);// matrix with all elements equal 1.2
+	Matrix<double> C(rows, cols, 3.6);// matrix with all elements equal 3.6
+	Matrix<double> D(rows, cols, 5.3);// matrix with all elements equal 5.3
+	Matrix<double> sum(rows, cols);// matrix with all elements equal 0
 
 	auto start = std::chrono::high_resolution_clock::now();
 	sum = 5.1*A + B - C + 2.1*D;
@@ -75,8 +77,8 @@ void example4()
 	std::cout << "example 4: " << std::endl;
 	size_t rows = 3;
 	size_t cols = 3;
-	ExpMatrix<short> A(rows, cols);
-	ExpMatrix<short> B(rows, cols,40);// matrix with all elements equal 40
+	Matrix<short> A(rows, cols);
+	Matrix<short> B(rows, cols,40);// matrix with all elements equal 40
 	//filling matrix A manually by index operator ()
 	A(0, 0) = 1; A(0, 1) = 2; A(0, 2) = 3;
 	A(1, 0) = 20; A(1, 1) = 5; A(1, 2) = 3;
@@ -100,10 +102,10 @@ void example5()
 	//example 5: matrix multiplication followed by addition with another matrix
 	std::cout << "example 5: " << std::endl;
 
-	ExpMatrix<double> A(100, 500, 4.1);// matrix with all elements equal 4.1
-	ExpMatrix<double> B(500, 100, 1.2);// matrix with all elements equal 1.2
-	ExpMatrix<double> C(100, 100, 3.6);// matrix with all elements equal 3.6
-	ExpMatrix<double> sum(100, 100);// matrix with all elements equal 0
+	Matrix<double> A(100, 500, 4.1);// matrix with all elements equal 4.1
+	Matrix<double> B(500, 100, 1.2);// matrix with all elements equal 1.2
+	Matrix<double> C(100, 100, 3.6);// matrix with all elements equal 3.6
+	Matrix<double> sum(100, 100);// matrix with all elements equal 0
 
 	auto start = std::chrono::high_resolution_clock::now();
 	sum = A * B + C;
@@ -113,6 +115,7 @@ void example5()
 	std::cout << "one sample entry of the output matrix : " << sum(0, 0) << std::endl;
 	std::cout << std::endl;
 }
+
 int main() 
 {
 	example1();
